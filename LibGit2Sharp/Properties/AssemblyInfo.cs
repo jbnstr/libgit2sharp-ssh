7d87bb1f728e7cf9de486e46ff1b7119826f9b86
Johan Boonstra <jboonstra@outlook.com>
2018-09-06 15:45:07 +0200

    Reference package 'LibGit2Sharp-SSH.NativeBinaries-libssh2-1.8.1.0-JBO'. Set version number to 0.23.0-JBO. Fixed path of xunit.runner.msbuild.dll.

--- LibGit2Sharp/Properties/AssemblyInfo.cs
+++ LibGit2Sharp/Properties/AssemblyInfo.cs
@@ -44,4 +44,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.23.0")]
 [assembly: AssemblyFileVersion("0.23.0")]
-[assembly: AssemblyInformationalVersion("0.23.0-dev00000000000000")]
+[assembly: AssemblyInformationalVersion("0.23.0-JBO")]
